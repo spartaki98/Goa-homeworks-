7e1df505a330d28e0a43be40c483a0a616e1c44f
spartaki98 <spartakphkhakadze@gmail.com>
2024-02-15 21:37:07 +0400

    second commit

---
+++ Day 2/homeworkss.py
@@ -0,0 +1,12 @@
+
+name= ('luka')
+surname= ("navrozashvili")
+print ("gavicani da davmegobrdi")
+
+
+
+print (name,surname)
+
+print ("goashi momwons swavlebis stili, did imedebs vamyareb goaze")
+
+
---
+++ Day 3/homework.py
@@ -0,0 +1,24 @@
+print("elisabedi  khambeshaia")
+print ("zaza fhkhakadze")
+print ("tamari tsutskiridze")
+
+
+
+print (zaza)
+print(zaza)
+print(;;;)
+print)dff
+print(kkk))
+print(sos))
+print(no))
+print(no))
+print sdds
+print(sss)
+prst(sss)
+print(goa)
+name+sss)
+print (sokol)
+print(5+f)
+print(saba=)
+
+
